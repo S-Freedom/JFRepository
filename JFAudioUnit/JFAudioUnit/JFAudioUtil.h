7a733208408d3aec750688512cc2a98fc898c44d
huangpengfei <445412679@qq.com>
2018-12-11 15:45:44 +0800

    add JFAudioUtil

---
+++ JFAudioUnit/JFAudioUnit/JFAudioUtil.h
@@ -0,0 +1,17 @@
+//
+//  JFAudioUtil.h
+//  JFAudioUnit
+//
+//  Created by 黄鹏飞 on 2018/12/11.
+//  Copyright © 2018 com.hpf. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface JFAudioUtil : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ JFAudioUnit/JFAudioUnit/JFAudioUtil.m
@@ -0,0 +1,13 @@
+//
+//  JFAudioUtil.m
+//  JFAudioUnit
+//
+//  Created by 黄鹏飞 on 2018/12/11.
+//  Copyright © 2018 com.hpf. All rights reserved.
+//
+
+#import "JFAudioUtil.h"
+
+@implementation JFAudioUtil
+
+@end
