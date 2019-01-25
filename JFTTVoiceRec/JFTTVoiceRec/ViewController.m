20010f5220f656e3934b319b4f9b9ed7e2008605
huangpengfei <445412679@qq.com>
2019-01-25 20:52:40 +0800

    增加提示错误次数

--- JFTTVoiceRec/JFTTVoiceRec/ViewController.m
+++ JFTTVoiceRec/JFTTVoiceRec/ViewController.m
@@ -11,6 +11,7 @@
 #import "DemoUtil.h"
 #import "NSString+Distance.h"
 
+static CGFloat weight = 25.0f;
 @interface ViewController ()
 
 @property (weak, nonatomic) IBOutlet UILabel *desLabel;
@@ -20,6 +21,8 @@
 @property (assign, nonatomic) NSInteger curIndex;   // 数组当前的index
 @property (nonatomic, strong) NSMutableArray *mArr; // 文章数组
 @property (nonatomic, strong) NSMutableArray *showArr; // 需要显示的临时数组
+@property (nonatomic, assign) NSInteger tipCount;
+@property (nonatomic, assign) BOOL tipFlag;
 @end
 
 @implementation ViewController
@@ -36,8 +39,10 @@
     if(sender.selected){
         
         self.resultPer = @"";
+        self.desLabel.text = @"";
         [self.showArr removeAllObjects];
         self.curIndex = 0;
+        self.tipCount = 0;
         __weak typeof(self) wSelf = self;
         [[JFOnlineVoiceRecoTool shareInstance] startHandler:^(JFTentcentHandler * _Nonnull handler) {
             __strong typeof(self) strongSelf = wSelf;
@@ -54,35 +59,69 @@
             NSString *resultStr = [DemoUtil filterIllegalChar:handler.text];
             NSInteger resultLen = resultStr.length;
             
-            // 获取当前index(curIndex)
+            // 获取当前index(curIndex)，
+            // 过滤文章当中的标点符号，以便更加准确的对比
             NSString *curStr = [DemoUtil filterIllegalChar:strongSelf.mArr[strongSelf.curIndex]];
             NSInteger curStrLen = curStr.length;
             
             while(resultLen >= curStrLen){
                 NSString *compareText = [resultStr substringWithRange:NSMakeRange(0, curStrLen)];
+                // 由于腾讯云返回的数据不准确，因而采取将汉字转换成拼音的形式进行对比
                 NSString *comPY = [DemoUtil convertHZ2PY:compareText];
                 NSString *curPY = [DemoUtil convertHZ2PY:curStr];
                 NSLog(@"comPY : %@", comPY);
                 NSLog(@"curPY : %@", curPY);
                 NSLog(@"%f", [comPY likePercent:curPY]);
-                if([comPY likePercent:curPY] >= 20.0f){
-                    // 显示lable
+                
+                // 如果是提示状态则删除最后一条，回归状态
+                if(strongSelf.tipFlag){
+                    [strongSelf.showArr removeLastObject];
+                    strongSelf.tipFlag = NO;
+                }
+                
+                // 由于返回数据不太准确，就算是拼音对比也不准确， 所以如果相似度超过了权重则默认正确
+                if([comPY likePercent:curPY] >= weight){
+                    
                     if(strongSelf.curIndex >= strongSelf.mArr.count) break;
+                    // 正确则增加到显示数组里面
                     [strongSelf.showArr addObject:strongSelf.mArr[strongSelf.curIndex]];
                     
+                    // 通过显示数组转换成字符串，显示到label上
                     strongSelf.desLabel.text = [strongSelf getResultText];
                     NSLog(@"%@", strongSelf.desLabel.text);
+                    // 每次正确，索引值+1
                     strongSelf.curIndex +=1;
+                    
+                    // 提示次数， 提示flag
+                    strongSelf.tipCount = 0;
+                    strongSelf.tipFlag = NO;
+                }else{
+                    // 错误一次 提示次数++
+                    strongSelf.tipCount ++;
+                }
+                
+                // 如果提示次数>=3，则增加提示文本
+                if(strongSelf.tipCount >= 3){
+                    
+                    if(strongSelf.curIndex+1 >= strongSelf.mArr.count) break;
+                    
+                    NSString *nextStr = [DemoUtil filterIllegalChar:strongSelf.mArr[strongSelf.curIndex+1]];
+                    NSInteger to = nextStr.length>2?1:0;
+                    NSString *tip = nextStr.length > 0? [nextStr substringToIndex:to]:@"";
+                    // 从下一条数据中增加1-2个字符到显示数组中，注意下次将这条数据从显示数组剔除
+                    [strongSelf.showArr addObject:tip];
+                    strongSelf.desLabel.text = [strongSelf getResultText];
+                    strongSelf.tipFlag = YES;
                 }
                 
                 if(strongSelf.curIndex >= strongSelf.mArr.count) break;
+                
+                // 递归，为了防止一次性录入太多的句子
                 resultStr = [resultStr substringFromIndex:curStrLen];
                 resultLen = resultStr.length;
                 curStr = [DemoUtil filterIllegalChar:strongSelf.mArr[strongSelf.curIndex]];
                 curStrLen = curStr.length;
             }
-            
-            
         } stateChange:^(VoiceState state) {
             NSLog(@"state : %@", @(state));
         }];
